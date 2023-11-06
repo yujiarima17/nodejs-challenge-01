999498f196031ba3d0cb1a6c78faf7ef1618f030
yujiarima17 <arimayuji2@gmail.com>
2023-11-06 19:55:48 -0300

    task class

---
+++ src/task.js
@@ -0,0 +1,16 @@
+export class Task{
+    id 
+    title
+    description
+    completed_at
+    created_at 
+    updated_at 
+    constructor(id, title, description, completed_at, created_at, updated_at){
+        id =id;
+        title = title
+        description = description
+        completed_at = completed_at
+        created_at = created_at
+        updated_at = updated_at
+      }
+}
