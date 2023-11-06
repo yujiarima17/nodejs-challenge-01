c276315cc00087484df4f7b712895b13543deef8
yujiarima17 <arimayuji2@gmail.com>
2023-11-06 08:57:47 -0300

    routes layout done

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+export const routes = [
+    {
+        method:'GET',
+        path : '',
+        handler :(request,response)=>{
+
+        }
+    },
+    {
+        method:'POST',
+        path : '',
+        handler :(request,response)=>{
+            
+        }
+    },
+    {
+        method:'DELETE',
+        path : '',
+        handler :(request,response)=>{
+            
+        }
+    },
+    {
+        method:'PATCH',
+        path : '',
+        handler :(request,response)=>{
+            
+        }
+    },
+    {
+        method:'PUT',
+        path : '',
+        handler :(request,response)=>{
+            
+        }
+    }
+]
